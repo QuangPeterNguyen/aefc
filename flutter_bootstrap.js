10ecee79a913cec18124f25cf5b4b9cf0af2c6ab
QuangPeterNguyen <quangpeternguyen@gmail.com>
2025-07-22 15:23:01 +0700

    Deploy aefc on Tue Jul 22 15:23:01 SEAST 2025

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2290504534"
+    serviceWorkerVersion: "751376221"
   }
 });
