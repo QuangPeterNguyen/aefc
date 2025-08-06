72c35da7f56f32d856fb5337e9f1339259643d73
QuangPeterNguyen <quangpeternguyen@gmail.com>
2025-08-06 22:46:56 +0700

    Deploy aefc on Wed Aug  6 22:46:56 SEAST 2025

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2819943577"
+    serviceWorkerVersion: "2866450231"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "5d61d6d01cea8ce6c693f6feae8bdb6e",
-"assets/AssetManifest.bin.json": "d3dc13e0796c600143a3584f995770d8",
-"assets/AssetManifest.json": "215bb328b2777075789e9224b95eb885",
+const RESOURCES = {"assets/AssetManifest.bin": "a9d8610e5775eea63797b8268abf1ad4",
+"assets/AssetManifest.bin.json": "536dff0fed252e5bc8bcda950de217aa",
+"assets/AssetManifest.json": "02219c35246d8b2507ac702338b70680",
 "assets/assets/icons/us.png": "ab291a3ffa8e81386f77c94056c06a5b",
 "assets/assets/icons/vn.png": "47e1003cdb97f364c0bafa85503330ec",
 "assets/assets/images/about_us_banner.png": "71e3b885371d22b1083329380c5918fe",
@@ -71,8 +71,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "5d61d6d01cea8ce6c693f6feae8bdb6e
 "assets/assets/images/players/vhieu.png": "71bc3fa94599bd7660fa2ce107aef307",
 "assets/assets/images/players/viethocons.png": "6b6b01d6a757feb7f96d890c1ae5ea4c",
 "assets/assets/images/players/viettien.png": "af10e9611934958c99b2055a3e22ebc9",
+"assets/assets/images/recent_scorers/backda.jpg": "89d0f2aa128a3bac440e1c2bd13738a2",
 "assets/assets/images/recent_scorers/chienloi.jpg": "857d4d097ca1c8995113413c820670ee",
 "assets/assets/images/recent_scorers/haohoang.jpg": "47e58a734fb54fa91d48928efb4b24f6",
+"assets/assets/images/recent_scorers/haule.jpg": "27f58883206b3dac4463ff2c29431e20",
+"assets/assets/images/recent_scorers/nguyenthuy.jpg": "be3c434c5c86c32b7dbad6328b98e776",
 "assets/assets/images/recent_scorers/quanglam.jpg": "e82a5d2f5b731bf04924520223d6b7f1",
 "assets/assets/images/recent_scorers/thoale.jpg": "4f45184a0579e465061278432c167381",
 "assets/assets/images/recent_scorers/trannguyenthailand.jpg": "47e58a734fb54fa91d48928efb4b24f6",
@@ -97,7 +100,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5d61d6d01cea8ce6c693f6feae8bdb6e
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "dc1ab8952179cb8791e4c704c30a560e",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "4c8540d84ea1b47c147d52a9eb750a05",
+"flutter_bootstrap.js": "b1023f86293fba29dbf5e9f324c54bdb",
 "icons/Icon-192.png": "da18a325f599482283c79563030ddaeb",
 "icons/Icon-512.png": "f860738f4681cc7e3e2d2c24020693a1",
 "icons/Icon-maskable-192.png": "da18a325f599482283c79563030ddaeb",
